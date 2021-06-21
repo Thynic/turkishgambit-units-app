1cb89f7a1976b7e5182378a98bd0612c65fa6f26
melih karakaya <karakayamelih01@gmail.com>
2021-06-22 01:32:06 +0300

    Initial commit

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,20 +2,22 @@ const fs = require('fs')
 const { google } = require('googleapis')
 
 const createFile = () => {
-    json = {
-        type: process.env.type,
-        project_id: process.env.project_id,
-        private_key_id: process.env.private_key_id,
-        private_key: process.env.private_key,
-        client_email: process.env.client_email,
-        client_id: process.env.client_id,
-        auth_uri: process.env.auth_uri,
-        token_uri: process.env.token_uri,
-        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-        client_x509_cert_url: process.env.client_x509_cert_url
-    }
+    // json = {
+    //     type: process.env.type,
+    //     project_id: process.env.project_id,
+    //     private_key_id: process.env.private_key_id,
+    //     private_key: process.env.private_key,
+    //     client_email: process.env.client_email,
+    //     client_id: process.env.client_id,
+    //     auth_uri: process.env.auth_uri,
+    //     token_uri: process.env.token_uri,
+    //     auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    //     client_x509_cert_url: process.env.client_x509_cert_url
+    // }
+
+    // json = process.env.
     
-    json = JSON.stringify(json)
+    // json = JSON.stringify(json)
 
     fs.writeFile('./creds.json', json, (err) => {
     if (!err) {
