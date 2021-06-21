2360927689e73c841adfeb53ae8c132e87ca6d1a
melih karakaya <karakayamelih01@gmail.com>
2021-06-22 00:14:37 +0300

    Initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,60 +1,61 @@
 const express = require('express')
 const router = new express.Router()
-const { google } = require('googleapis')
+const {createFile, authorize} = require('./auth')
 
 let nicknames = []
 
-router.post('', async (req, res) => {
-    const { unitCode, min } = req.body
+createFile()
 
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "creds.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    })
+router.post('', authorize, async (req, res) => {
+
+    try {
+      const { unitCode, min } = req.body  
     
-      // Create client instance for auth
-      const client = await auth.getClient()
-    
-      // Instance of Google Sheets API
-      const googleSheets = google.sheets({ version: "v4", auth: client })
-    
-      const spreadsheetId = "1HYcx0XSM8QBiU24J0qomFbMZA7us45cpug_4nAtG0yQ"
-  
       // Get metadata about spreadsheet
-      const unitData = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: `'Oyuncu Verileri'!${unitCode}:${unitCode}`
-      })
+        const unitData = await req.googleSheets.spreadsheets.values.get({
+          auth: req.auth,
+          spreadsheetId: req.spreadsheetId,
+          range: `'Oyuncu Verileri'!${unitCode}:${unitCode}`
+        })
+  
+        const nickData = await req.googleSheets.spreadsheets.values.get({
+          auth: req.auth,
+          spreadsheetId: req.spreadsheetId,
+          range: `'Oyuncu Verileri'!A:A`
+        })
+        
+        if (unitCode && min) {
+          let count = 1
+          const unitName = unitData.data.values[0]
+          nicknames[0] = unitName + ': '
 
-      const nickData = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: `'Oyuncu Verileri'!A:A`
-      })
-
-      let count = 1
-      const unitName = unitData.data.values[0]
-      nicknames[0] = unitName + ': '
-      unitData.data.values.forEach((level, index) => {
-        if (parseInt(level) >= min ||
-            (!parseInt(level) && parseInt(level.toString().length) < 5) &&
-            level != ''
-           ) {
-                nicknames.push(count + '. ' + nickData.data.values[index])
-                count++ 
+          unitData.data.values.forEach((level, index) => {
+            if (parseInt(level) >= min ||
+                ((!parseInt(level) && level != '') && (level.toString().toLowerCase() === 'max'))
+              )
+                {
+                    nicknames.push(count + '. ' + nickData.data.values[index])
+                    count++ 
+            }
+          })
         }
-      })
 
-      res.redirect('/')
+        res.redirect('/')
+    } catch (e) {
+      console.log(e)
+    }
 })
 
 router.get('/metadata', (req, res) => {
-  if (nicknames.length === 1) {
+  try {
+    if (nicknames.length === 1) {
       nicknames[1] = 'yoktur'
+    }
+    res.send(nicknames)
+    nicknames = []
+  } catch {
+    throw new Error ('get metadata error')
   }
-  res.send(nicknames)
-  nicknames = []
 })
 
 module.exports = router
---
+++ public/js/auth.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const { google } = require('googleapis')
+
+const createFile = () => {
+    json = {
+        type: process.env.type,
+        project_id: process.env.project_id,
+        private_key_id: process.env.private_key_id,
+        private_key: process.env.private_key,
+        client_email: process.env.client_email,
+        client_id: process.env.client_id,
+        auth_uri: process.env.auth_uri,
+        token_uri: process.env.token_uri,
+        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url: process.env.client_x509_cert_url
+    }
+    
+    json = JSON.stringify(json)
+
+    fs.writeFile('./creds.json', json, (err) => {
+    if (!err) {
+        console.log('created')
+    }
+    })
+}
+
+
+const authorize = async (req, res, next) => {
+    try {
+        
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "creds.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets"
+        })
+        
+        // Create client instance for auth
+        const client = await auth.getClient()
+  
+        // Instance of Google Sheets API
+        const googleSheets = google.sheets({ version: "v4", auth: client })
+      
+        const spreadsheetId = "1HYcx0XSM8QBiU24J0qomFbMZA7us45cpug_4nAtG0yQ"
+
+        req.auth = auth
+        req.googleSheets = googleSheets
+        req.spreadsheetId = spreadsheetId
+
+        next()
+    } catch {
+        throw new Error('authentication failed.')
+    }
+
+}
+
+module.exports = {
+    authorize,
+    createFile
+}
+
